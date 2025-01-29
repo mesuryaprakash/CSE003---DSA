311dc15ca56396f3f4a80e1b6e7e7e222ad9a39c
theboysurya <thesuryasingh9@gmail.com>
2025-01-29 09:08:56 +0530

    till circular linked list

--- circularLinkedList.cpp
+++ circularLinkedList.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 using namespace std;
 
+
 class Node{
     public:
-    int data;
+    Node * prev;
     Node * next;
+    int data;
 
     Node(int val){
         data = val;
@@ -12,100 +14,52 @@ class Node{
     }
 };
 
-void insertAtTail(Node* &head, int val){
+void insertAtTail(Node* &head,int val){
     Node * n = new Node(val);
 
     if(head == NULL){
         head = n;
+        head->next = n;
         return;
     }
 
     Node * temp = head;
-    while(temp->next != NULL){
+    while(temp->next != head){
         temp = temp->next;
     }
 
     temp->next = n;
+    n->next = head;
 
 }
 
 void insertAtHead(Node* &head, int val){
     Node * n = new Node(val);
+
+    if(head == NULL){
+        head = n;
+        head->next = head;
+        return;
+    }
+
+    Node * temp = head;
+    while(temp->next != head){
+        temp = temp->next;
+    }
+    temp->next = n;
     n->next = head;
+
     head = n;
 }
 
-void display(Node * head){
+void display(Node* &head){
     Node * temp = head;
-    while (temp != NULL)
-    {
-        cout << temp-> data << " -> ";
+    do {
+        cout << temp->data << " -> ";
         temp = temp->next;
-    }
-    cout << "NULL" << endl;
+    } while(temp != head);
+    cout << "Head" << endl;
 }
-
-bool search(Node * head, int key){
-    Node * temp = head;
-    while(temp != NULL){
-        if(temp->data == key){
-            return true;
-        }
-        temp = temp->next;
-    }
-    return false;
-}
-
-void deletion(Node * &head, int key){
-    Node * temp = head;
-    if(head == NULL) return;
-    while(temp->next->data != key){
-        temp = temp->next;
-    }
-    Node* del = temp->next;
-    temp->next = temp->next->next;
-    delete del;
-}
-
-void delAtHead(Node* &head){
-    Node * temp = head;
-    head = head->next;
-    delete temp;
-}
-
-//Node*
-Node* reversal(Node* head){
-    Node * prev = NULL;
-    Node * curr = head;
-    Node * next = NULL;
-
-    while(curr != NULL){
-        next = curr->next;
-        curr->next = prev;
-
-        prev = curr;
-        curr = next;
-    }
-    return prev;
-}
-
-Node* kReversal(Node* &head, int k){
-    Node * prev = NULL;
-    Node * curr = head;
-    Node * next = NULL;
-    int count = 0;
-
-    while(count < k){
-        next = curr -> next;
-        curr -> next = prev;
-
-        prev = curr;
-        curr = next;
-        count++;
-    }
-    kReversal(curr, k);
-}
-
 
 Node* checkLoop(Node* head){
     if(head == NULL) return NULL;
@@ -153,18 +107,47 @@ Node* getStartingNode(Node* head){
         intersection = intersection->next;
     }
 
+    cout << slow->data;
+
+
     return slow;
 }
 
+void removeLoop(Node* &head){
+    if(head == NULL) return;
+
+    Node* startOfLoop = getStartingNode(head);
+    Node* temp = startOfLoop;
+
+    while(temp -> next != startOfLoop){
+        temp = temp->next;
+    }
+    temp->next = NULL;
+}
+
+void delAtHead(Node* &head){
+    Node * del = head;
+    Node * temp = head;
+
+    while(temp->next != head){
+        temp = temp->next;
+    }
+    temp->next = head->next;
+    head = head->next;
+    delete del;
+}
+
+
 
 int main(){
-    Node * head = NULL;
-    insertAtHead(head, 1);
-    insertAtHead(head, 2);
-    insertAtTail(head, 10);
-    insertAtTail(head, 20);
-    display(head);
-    // Node * n = reversal(head);
-    // display(n);
-
+    Node* head = NULL;
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtHead(head, 10);
+    // display(head);
+    getStartingNode(head);
+    // delAtHead(head);
+    // cout << endl;
+    // display(head);
 }
+//delete at nth position;
