690012b384ecb7537cbe17f08a8ade4280b967d8
theboysurya <thesuryasingh9@gmail.com>
2025-02-06 00:07:28 +0530

    tree traversal completed

--- tree.cpp
+++ tree.cpp
@@ -14,6 +14,37 @@ class Node{
     }
 };
 
+void inorder(Node* root){
+    if(root == NULL) return;
+
+    inorder(root->left);
+
+    cout << root->data << " ";
+
+    inorder(root->right);
+
+}
+
+void preorder(Node* root){
+    if(root == NULL) return;
+
+    cout << root->data << " ";
+
+    preorder(root -> left);
+
+    preorder(root -> right);
+}
+
+void postorder(Node* root){
+    if(root == NULL) return;
+
+    postorder(root -> left);
+
+    postorder(root -> right);
+
+    cout << root -> data << " ";
+}
+
 int main(){
     Node* root = new Node(1);
     root->left = new Node(2);
@@ -22,6 +53,11 @@ int main(){
     root->left->right = new Node(5);
     root->right->left = new Node(6);
     root->right->right = new Node(7);
-    
+    inorder(root);
+    cout << endl;
+    preorder(root);
+    cout << endl;
+    postorder(root);
+
     return 0;
 }
